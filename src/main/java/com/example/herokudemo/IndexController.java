95e306e054d1d57751dd33ea283f9742d02e8299
Nguyễn Tuân <nguyenvantuan2479@gmail.com>
2021-01-20 20:16:03 +0700

    KJKJKJ

--- src/main/java/com/example/herokudemo/IndexController.java
+++ src/main/java/com/example/herokudemo/IndexController.java
@@ -8,6 +8,6 @@ public class IndexController {
 
     @GetMapping("/")
     public String index() {
-        return "Hello there! I'm running.";
+        return "Hello there! I'm running. HELLO";
     }
 }
